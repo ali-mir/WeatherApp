8c4ac6be677203186fcf57a3f05284790fdce7f2
Ali Mir <ali.ahmed.mir@gmail.com>
2016-08-04 15:24:22 -0700

    Added web content download

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userCity: UITextField!
+    
+    @IBOutlet var weatherLabel: UILabel!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
+        
+        if url != nil {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                
+                if error == nil {
+                    
+                    let urlContent = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    print(urlContent)
+                    
+                } else {
+                    self.weatherLabel.text = "Network error. Please try again later"
+                    
+                }
+                
+                
+            })
+            
+            task.resume()
+            
+        } else {
+            weatherLabel.text = "Please enter a valid city name!"
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
